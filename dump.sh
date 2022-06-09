0f1b59511c27623f21aa9ed98cd82ef1e0171fe4
r1cc4rdo <r1cc4rdo@users.noreply.github.com>
2022-06-08 21:09:08 -0700

    Update dump.sh

--- dump.sh
+++ dump.sh
@@ -1,2 +1,3 @@
-things-cli -j all > things.json
-things-cli -j --recursive all > things_recursive.json
+wget https://github.com/thingsapi/things-cli/raw/master/tests/main.sqlite -o test_things.sqlite
+things-cli -d test_things.sqlite -j --recursive all > test_things.json
+python convert.py --output test_vault test_things.json
